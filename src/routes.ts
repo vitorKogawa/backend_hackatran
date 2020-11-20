44f7d421a36d7c22c3cd4ec279e8170cee5e1312
vitorKogawa <v245582@dac.unicamp.br>
2020-11-20 03:11:33 -0300

    alteração no arquivo de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -5,15 +5,15 @@ import AlternativaController from './controllers/AlternativaController'
 
 const router = Router()
 
-//ROTAS PARA GERENCIAMENTO DAS INFRACÇÕES.
+//ROTAS PARA GERENCIAMENTO DAS INFRACÇÕES
 router.post('/infracao', InfracaoController.store)
 router.get('/infracao', InfracaoController.index)
 
-//ROTAS PARA GERENCIAMENTO DAS PERGUNTAS.
+//ROTAS PARA GERENCIAMENTO DAS PERGUNTAS
 router.post('/pergunta', PerguntaController.store)
 router.get('/pergunta', PerguntaController.index)
 
-//ROTAS PARA GERENCIAMENTO DAS ALTERNATIVAS.
+//ROTAS PARA GERENCIAMENTO DAS ALTERNATIVAS
 router.post('/alternativa', AlternativaController.store)
 router.get('/alternativa/:id_pergunta', AlternativaController.show)
 
